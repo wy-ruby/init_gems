4fd0b88ac082708d1a81792c344ba23045410442
王允 <906971957@qq.com>
2019-05-28 18:10:31 +0800

    配置whenever的日志输出目录

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,7 +20,7 @@
 # Learn more: http://github.com/javan/whenever
 # 可以这样设置全局执行日志
 # set :output, {:error => 'log/cron_error.log', :standard => 'cron_access.log'}
-set :output, File.expand_path(File.dirname(__FILE__) + "../log/whenever.log")
+set :output, "#{shared_path}/log/cron_log.log"
 
 every '* * * * *' do
   rake 'practice:whenever_test'
