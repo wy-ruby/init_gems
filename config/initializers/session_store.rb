cf65ad59c18ceed4cec43f74e4d77b19a30b5759
王允 <906971957@qq.com>
2019-05-29 18:19:40 +0800

    测试配置的redis的超时时间

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :redis_store,
-                                       servers: CACHE_REDIS_CONFIG.merge({namespace: 'session', expire_after: 1.minutes}),
+                                       servers: CACHE_REDIS_CONFIG.merge({namespace: 'session'}),
                                        expire_after: 90.minutes,
                                        key: "#{Rails.application.class.parent_name.downcase}_session_#{Time.now.to_i}",
                                        threadsafe: false
