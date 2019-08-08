d4843f443f59209e48966b6ef3940c04187e2b5a
王允 <906971957@qq.com>
2019-08-08 18:31:34 +0800

    调试静态文件的伺服方式

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,5 +2,5 @@
 // 在admin.js预编译文件中引入了自身和lib/assets/javascripts/check.js文件，注意该文件不会体现在控制台的head中，
 // 控制台中只会显示admin这个编辑后的文件。
 //= require_self
-//= require check
+// require check
 document.write("the file in app/assets/javascripts/admin.js");
